cae1adf96f748867cdd68ad119ddd8ccd8320c69
huangweiye <1304610938@qq.com>
2025-01-03 17:27:13 +0800

    Update index.ts
    调试

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,7 +23,7 @@ export const constantRouterMap: AppRouteRecordRaw[] = [
     children: [
       {
         path: '/redirect/:path(.*)',
-        name: 'Redirect',
+        name: 'Redirect11',
         component: () => import('@/views/Redirect/Redirect.vue'),
         meta: {}
       }
